77534f797bb98a1ccfe5d1406e1d2d8afad9b30c
imadelcass <imadelcass@gmail.com>
2023-06-07 11:09:35 +0100

    Add analytics

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,17 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+import { event } from 'vue-gtag'
+import { useRoute } from 'vue-router'
+import { onMounted, watch } from 'vue'
+
+const route = useRoute()
+
+onMounted(() => {
+  event('App Mounted')
+})
+
+watch(route, (newVal) => {
+  event('Route Change', newVal.name)
+})
 </script>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,22 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(
+  VueGtag,
+  {
+    appName: 'My application',
+    pageTrackerScreenviewEnabled: true,
+    config: { id: 'G-0TJZKY5TNF' }
+  },
+  router
+)
 app.use(createPinia())
 app.use(ElementPlus)
 app.use(router)
